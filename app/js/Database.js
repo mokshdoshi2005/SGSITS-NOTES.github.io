d9c5eb8bc58bd1b11a24677071bf83c33f54cae6
Moksh Doshi <94376264+mokshdoshi2005@users.noreply.github.com>
2023-06-19 23:49:26 +0530

    Add files via upload

---
+++ app/js/Database.js
@@ -0,0 +1,53 @@
+const database ={
+    list : ["course1","course2","course3","course4","course5","course6","course7","course8"],
+    "course1" : {
+        "link": 'https://dribbble.com/shots/21192792-details-girl',
+        "name": 'course1',
+        "subtext": 'demo',
+        "img": "https://cdn.dribbble.com/userupload/6235429/file/original-c098fec3586d97b183bb40c00351701f.png?compress=1&resize=752x"
+    },
+    "course2" : {
+        "link": 'https://dribbble.com/shots/21192792-details-girl',
+        "name": 'course2',
+        "subtext": 'demo',
+        "img": "https://cdn.dribbble.com/userupload/6235429/file/original-c098fec3586d97b183bb40c00351701f.png?compress=1&resize=752x"
+    },
+    "course3" : {
+        "link": 'https://dribbble.com/shots/21192792-details-girl',
+        "name": 'course3',
+        "subtext": 'demo',
+        "img": "https://cdn.dribbble.com/userupload/6235429/file/original-c098fec3586d97b183bb40c00351701f.png?compress=1&resize=752x"
+    },
+    "course4" : {
+        "link": 'https://dribbble.com/shots/21192792-details-girl',
+        "name": 'course4',
+        "subtext": 'demo',
+        "img": "https://cdn.dribbble.com/userupload/6235429/file/original-c098fec3586d97b183bb40c00351701f.png?compress=1&resize=752x"
+    },
+    "course5" : {
+        "link": 'https://dribbble.com/shots/21192792-details-girl',
+        "name": 'course5',
+        "subtext": 'demo',
+        "img": "https://cdn.dribbble.com/userupload/6235429/file/original-c098fec3586d97b183bb40c00351701f.png?compress=1&resize=752x"
+    },
+    "course6" : {
+        "link": 'https://dribbble.com/shots/21192792-details-girl',
+        "name": 'course6',
+        "subtext": 'demo',
+        "img": "https://cdn.dribbble.com/userupload/6235429/file/original-c098fec3586d97b183bb40c00351701f.png?compress=1&resize=752x"
+    },
+    "course7" : {
+        "link": 'https://dribbble.com/shots/21192792-details-girl',
+        "name": 'course7',
+        "subtext": 'demo',
+        "img": "https://cdn.dribbble.com/userupload/6235429/file/original-c098fec3586d97b183bb40c00351701f.png?compress=1&resize=752x"
+    },
+    "course8" : {
+        "link": 'https://dribbble.com/shots/21192792-details-girl',
+        "name": 'course8',
+        "subtext": 'demo',
+        "img": "https://cdn.dribbble.com/userupload/6235429/file/original-c098fec3586d97b183bb40c00351701f.png?compress=1&resize=752x"
+    },
+};
+export default database;
+/* list must contain course name and there must be a corse name in form of a dictionary*/ 
---
+++ app/js/notes.js
@@ -0,0 +1,47 @@
+import database from "./Database.js";
+console.log(database)
+
+const WRAPPER = document.getElementById("wrapper")
+
+for (let index = 0; index < database['list'].length; index++) {
+    createCard(WRAPPER, index);
+    let current_course = database["list"][index]
+    addData(WRAPPER.children[index], WRAPPER.children[index].childNodes, database[`${current_course}`])
+}
+
+function addData(parent, ary, dict) {
+        
+    let card_nth_child = ary;
+    console.log(parent)
+    parent.href = dict["link"]
+    // COURSE NAME
+    card_nth_child[1].textContent = dict["name"];
+    
+    // SUBTEXT
+    card_nth_child[2].textContent = dict["subtext"];
+    
+    // IMAGE
+    card_nth_child[0].src = dict['img'];
+    
+}
+
+
+function createCard(wrapper, index){
+    let card = document.createElement("a");
+    card.classList.add("card");
+    card.dataset.index = index;
+
+    {let icon = document.createElement("img");
+    icon.classList.add("icon");
+    card.appendChild(icon);
+    let name = document.createElement("h1");
+    name.classList.add("Course-name");
+    name.classList.add("SHORT-STACK");
+    card.appendChild(name);
+    let subtext = document.createElement("div");
+    subtext.classList.add("Subtext");
+    subtext.classList.add("SHORT-STACK");
+    card.appendChild(subtext);};
+
+    wrapper.appendChild(card)
+}
